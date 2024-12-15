3af2ea7e1d109aab71a0d463e0b73b66c8895186
Sovannara Tav <129139304+SovannaraTav@users.noreply.github.com>
2024-12-14 17:11:31 -0800

    Transform WebSocketClient to NetworkManager class to handle separation between networking and game logic

--- client.js
+++ client.js
@@ -14,45 +14,93 @@ d) My CSS 481 course textbook on web sockets (Chapter 8, Section 12)
     (https://www.zybooks.com/catalog/web-programming/)
 */
 
-// WebSocket client information
+class NetworkManager {
+    // Private data attributes
+    #webSocketHost;
+    #webSocketPortNumber;
+    #webSocketClient
+
+    // Class constructor
+    constructor(webSocketHost, webSocketPortNumber) {
+        this.#webSocketHost = webSocketHost;
+        this.#webSocketPortNumber = webSocketPortNumber;
+        this.#webSocketClient = null;
+    }
+
+    connectToServer() {
+        /*
+        Establishes a WebSocket client to connect to the WebSocket server through 
+        the NetworkManager
+        */
+        this.#webSocketClient = 
+            new WebSocket(`ws://${this.#webSocketHost}:${this.#webSocketPortNumber}`);
+
+        /*
+        Handles the event when the WebSocket client is connected to the WebSocket 
+        server through the NetworkManager
+        */
+        this.#webSocketClient.onopen = () => {
+            console.log(
+                "[NetworkManager] - WebSocket client connected to WebSocket server!");
+            document.getElementById("status").innerText = 
+                "WebSocket client connected to WebSocket server!";
+        };
+
+        /*
+        Handles the event when receiving data from the WebSocket server to the 
+        WebSocket client through the NetworkManager
+        */
+        this.#webSocketClient.onmessage = (event) => {
+            console.log(
+                `[NetworkManager] - WebSocket client received data from WebSocket server: ${event.data}`);
+        };
+
+        /*
+        Handles the event when the WebSocket client disconnects from the WebSocket 
+        server through the NetworkManager
+        */
+        this.#webSocketClient.onclose = () => {
+            console.log("[NetworkManager] - WebSocket client disconnected from WebSocket server!");
+        }
+
+        /*
+        Handles the event when there are errors from the WebSocket client through 
+        the NetworkManager
+        */
+        this.#webSocketClient.onerror = (error) => {
+            console.error(`[NetworkManager] - WebSocket Client Error: ${error}`);
+            document.getElementById("status").innerText = 
+                "WebSocket client error connecting to WebSocket server";
+        };
+    }
+
+    /*
+    Function to allow the WebSocket client to send a message to the WebSocket 
+    server through the NetworkManager
+    */
+    send(message) {
+        if (this.#webSocketClient && 
+            this.#webSocketClient.readyState === WebSocket.OPEN) {
+            console.log(
+                `[NetworkManager] - WebSocket client sending message to WebSocket server: ${message}`);
+            this.#webSocketClient.send(message);
+        }
+    }
+}
+
+// Instantiating the NetworkManager to separate networking from game logic
 const webSocketPortNumber = 8081;
 const webSocketHost = "localhost";
-
-// Establishes a WebSocket client to connect to the WebSocket server
-const webSocketClient = 
-    new WebSocket(`ws://${webSocketHost}:${webSocketPortNumber}`);
+const networkManager = new NetworkManager(webSocketHost, webSocketPortNumber);
+networkManager.connectToServer();
 
 /*
-Handles the event when the WebSocket client is connected to the WebSocket server
-*/
-webSocketClient.onopen = () => {
-    console.log("[WebSocket Client] - Connected to WebSocket Server!");
-    document.getElementById("status").innerText = 
-        "WebSocket client connected to Websocket server!";
-};
-
-/*
-Handles the event when receiving data from the WebSocket server to the WebSocket 
-client
-*/
-webSocketClient.onmessage = (event) => {
-    console.log(
-        `[WebSocket Client] - Received data from WebSocket server: ${event.data}`);
-};
-
-// Handles the event when there are errors from the WebSocket client
-webSocketClient.onerror = (error) => {
-    console.error(`[WebSocket Client] - Error: ${error}`);
-    document.getElementById("status").innerText = 
-        "WebSocket client error connecting to WebSocket server";
-};
-
-/*
-Function to allow the WebSocket client to send a message to the WebSocket server
+Function to allow the WebSocket client to send a message to the WebSocket server 
+through the NetworkManager
 */
 function sendMessage() {
     let message = document.getElementById("messageInput").value;
     console.log(
-        `[WebSocket Client] - Sending message to WebSocket server: ${message}`);
-    webSocketClient.send(message);
+        `[NetworkManager] - WebSocket client sending message to WebSocket server: ${message}`);
+    networkManager.send(message);
 }
